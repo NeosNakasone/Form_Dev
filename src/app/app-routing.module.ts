3e3a474afc5e610a4834c326da407682cfe0b33b
Neos Nakasone <neosnakasone@gmail.com>
2022-06-28 15:02:28 +0200

    ajout co

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { PlanningComponent } from './planning/planning.component';
+import { ParticipantComponent } from './participant/participant.component';
+import { SignatureComponent } from './signature/signature.component';
+import { ProfesseurComponent } from './professeur/professeur.component';
+import { AuthGuard } from './auth.guard';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  //{ path: '', pathMatch: 'full', redirectTo: 'connexion'},
+ // { path: 'auth', component: AuthGuard },
+  { path: 'connexion', component: ConnexionComponent, /*canActivate:[AuthGuard] */},
+  { path: 'professeur', component: ProfesseurComponent },
+  { path: 'planning', component: PlanningComponent },
+  { path: 'participant', component: ParticipantComponent },
+  { path: 'signature', component: SignatureComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { PlanningComponent } from './planning/planning.component';
+import { ParticipantComponent } from './participant/participant.component';
+import { SignatureComponent } from './signature/signature.component';
+import { ProfesseurComponent } from './professeur/professeur.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConnexionComponent,
+    ProfesseurComponent,
+    PlanningComponent,
+    ParticipantComponent,
+    SignatureComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,//import pour formulaire reactif
+    ReactiveFormsModule,//idem en haut
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
