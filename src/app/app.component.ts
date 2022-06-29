722b9346226b70944e563afeef63af7ec026dde9
mromain <romain.matteo13@gmail.com>
2022-06-29 16:53:23 +0200

    Ajout d'un message pour dire que l'on est déconnecté

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {fromEvent, map, of, Subscription} from "rxjs";
+import {merge} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Form_Dev';
+  networkStatus: boolean = false;
+  networkStatus$: Subscription = Subscription.EMPTY;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.checkNetworkStatus();
+  }
+
+  ngOnDestroy(): void {
+    this.networkStatus$.unsubscribe();
+  }
+
+  checkNetworkStatus() {
+    this.networkStatus = navigator.onLine;
+    this.networkStatus$ = merge(
+      of(null),
+      fromEvent(window, 'online'),
+      fromEvent(window, 'offline')
+    )
+      .pipe(map(() => navigator.onLine))
+      .subscribe(status => {
+        console.log('status', status);
+        this.networkStatus = status;
+      });
+  }
 }
